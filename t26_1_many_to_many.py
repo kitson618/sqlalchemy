9b81d138f44c980b076c837e1a8e571dbb146dd2
kitson618 <kitson618c@gmail.com>
2019-02-08 08:51:09 +0000

    Finished up to t26

---
+++ t26_1_many_to_many.py
@@ -0,0 +1,127 @@
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, create_engine, Sequence, ForeignKey
+from sqlalchemy.orm import sessionmaker, relationship
+from sqlalchemy import Table, Text
+
+Base = declarative_base()
+
+# configure Session class with desired options
+Session = sessionmaker()
+
+# later, we create the engine
+engine = create_engine('sqlite:///:memory:', echo=True)
+
+# associate it with our custom Session class
+Session.configure(bind=engine)
+
+# work with the session
+session = Session()
+
+# User Table
+class User(Base):
+    __tablename__ = 'users'
+  
+    id = Column(Integer, Sequence('user_id_seq'), primary_key=True)
+    name = Column(String(50))
+    fullname = Column(String(50))
+    password = Column(String(12))
+    addresses = relationship("Address", back_populates='user', cascade="all, delete, delete-orphan")
+
+    def __repr__(self):
+        return "<User(name='%s', fullname='%s', password='%s')>" % (self.name, self.fullname, self.password)
+
+# Address Table
+class Address(Base):
+    __tablename__ = 'addresses'
+    id = Column(Integer, primary_key=True)
+    email_address = Column(String, nullable=False)
+    user_id = Column(Integer, ForeignKey('users.id'))
+    user = relationship("User", back_populates="addresses")
+
+    def __repr__(self):
+        return "<Address(email_address='%s')>" % self.email_address
+
+# Create an association table 'post_keywords'
+post_keywords = Table('post_keywords', Base.metadata,
+    Column('post_id', ForeignKey('posts.id'), primary_key=True),
+    Column('keyword_id', ForeignKey('keywords.id'), primary_key=True)
+)
+
+# Create a Class 'BlogPost'
+class BlogPost(Base):
+    __tablename__ = 'posts'
+
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('users.id'))
+    headline = Column(String(255), nullable=False)
+    body = Column(Text)
+
+    # many to many BlogPost<->Keyword
+    keywords = relationship('Keyword',secondary=post_keywords,back_populates='posts')
+
+    def __init__(self, headline, body, author):
+        self.author = author
+        self.headline = headline
+        self.body = body
+
+    def __repr__(self):
+        return "BlogPost(%r, %r, %r)" % (self.headline, self.body, self.author)
+
+# Create a Class 'Keyword'
+class Keyword(Base):
+    __tablename__ = 'keywords'
+
+    id = Column(Integer, primary_key=True)
+    keyword = Column(String(50), nullable=False, unique=True)
+    posts = relationship('BlogPost',secondary=post_keywords,back_populates='keywords')
+
+    def __init__(self, keyword):
+        self.keyword = keyword
+
+# Create the bidirectional relationship BlogPost <--> User  
+BlogPost.author = relationship(User, back_populates="posts")
+User.posts = relationship(BlogPost, back_populates="author", lazy="dynamic")
+
+# create the database
+Base.metadata.create_all(engine)
+
+# insert data for User & Address 
+session.add_all([
+    User(name='jack', fullname='Jack Bean', password='gjffdd'),
+    User(name='ed', fullname='Ed Jones', password='edspassword'),
+    User(name='wendy', fullname='Wendy Williams', password='foobar'),
+    User(name='mary', fullname='Mary Contrary', password='xxg527'),
+    User(name='fred', fullname='Fred Flinstone', password='blah'),
+    Address(email_address='jack@google.com', user_id=1),
+    Address(email_address='j25@yahoo.com', user_id=1)])
+
+# write to database
+session.commit()
+
+
+
+
+
+
+
+
+
+
+
+
+
+# create the database 
+Base.metadata.create_all(engine)
+
+# insert data for User & Address 
+session.add_all([
+    User(name='jack', fullname='Jack Bean', password='gjffdd'),
+    User(name='ed', fullname='Ed Jones', password='edspassword'),
+    User(name='wendy', fullname='Wendy Williams', password='foobar'),
+    User(name='mary', fullname='Mary Contrary', password='xxg527'),
+    User(name='fred', fullname='Fred Flinstone', password='blah'),
+    Address(email_address='jack@google.com', user_id=1),
+    Address(email_address='j25@yahoo.com', user_id=1)])
+
+# write to database
+session.commit()
